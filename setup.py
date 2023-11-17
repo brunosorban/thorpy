5317530eecefa30980bff8b8275f16922c907998
Bruno Sorban <68908065+brunosorban@users.noreply.github.com>
2023-11-17 07:00:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="hopper_traj",
-    version="0.1.1",
+    name="thorpy",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
